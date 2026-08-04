5ce7badac37d7146429307112bdd51eb3aaa40a4
Gitpho <gitpho@gmail.com>
2026-08-04 16:27:35 -0400

    5.3 Aggregate | Docs

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -1,12 +1,12 @@
 -- +goose Up
-CREATE TABLE feed_follows (
+CREATE TABLE feeds (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
-    UNIQUE (user_id, feed_id)
+    name TEXT NOT NULL,
+    url TEXT NOT NULL UNIQUE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
-DROP TABLE feed_follows;
+DROP TABLE feeds;
